a2d395af2392f23f5a0905e12fcab2e6960da131
Mason <mkrug6@wgu.edu>
2021-05-21 17:58:26 -0700

    Removed pandas import

--- download.py
+++ download.py
@@ -1,76 +1,27 @@
 import yfinance as yf
 from datetime import date
-from config import *
-import pandas as pd
 import os
 
-print(path)
-print(config.path)
+#Stocks that you want to download
+ticker = ["SPY", "GOOG", "MSFT", "TSLA"]
+
+#Path to save file directory
+path = r'./Data/'
 
 #Sets start date for stock datetime
 today = date.today()
 start = date(2020,1,1)
 start = start.strftime("%Y-%m-%d")
-print(start)
 
 #Sets end date for stock datetime
 end = today.strftime("%Y-%m-%d")
-print(end)
 
+#Fill the df with date. Takes in ticker as a dictionary/list
+def download_csv(start, end, ticker):
+    for i in ticker:
+        stock = []
+        stock = yf.download(i,start=start, end=end)
+        stock.to_csv (path + i + '.csv', index = True, header=False)
+    return
 
-#Tickers to download
-ticker = ["SPY", "GOOG", "MSFT", "TSLA"]
-print (ticker)
-
-
-#Empty df to iterate over
-stock = []
-
-#Fill the df with date
-
-for i in ticker:
-    stock = yf.download(i,start=start, end=end)
-    stock.to_csv (path + i + '.csv', index = True, header=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-p = Path('./')
-input_file.to_csv(Path(p, 'match_' + filename + '.csv')), index=False)
-
-
-
-"""
-for i in ticker:
-    print(i)
-
-    stock = []
-    stock = yf.download(i,start=start, end=end)
-    stock.head()
-"""
-
-
-
-
-
-
-
-
-
-
-
-
-
-#def download_csv(ticker, date_start):
-#    data = yf.download(ticker, date_start)
-#
-#   return data
+download_csv(start, end, ticker)
